e4bbffe469a318c2ad792d6e078834fd1dc0827f
gotcadumitru <64309696+gotcadumitru@users.noreply.github.com>
2021-03-27 09:34:30 +0200

    add signup 2

--- src/pages/SignInUp/Signinup.jsx
+++ src/pages/SignInUp/Signinup.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/SignInComponent';
-// import SignUp from '../../components/sign-up/SignUp';
+import SignUp from '../../components/sign-up/SignUp';
 import './Signinup.scss';
 import hotelIMG from '../../assets/images/himage.png'
 
